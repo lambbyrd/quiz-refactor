07381c602912dac8907b94f8c3411899ce3ad85f
Codepressd <creederone@gmail.com>
2016-08-24 11:35:01 -0700

    Everything still works but don't think this is what they were looking for

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var QUESTIONS = [
-    {
+/*move all items we will store into the state object*/
+
+var state = {
+
+    questions: [{
         text: '<:48:x<:65:=<:6C:$=$=$$~<:03:+$~<:ffffffffffffffbd:+$<:ffffffffffffffb1:+$<:57:~$~<:18:x+$~<:03:+$~<:06:x-$x<:0e:x-$=x<:43:x-$',
         answers: [
             '0815',
@@ -8,8 +11,7 @@ var QUESTIONS = [
             'Barely'
         ],
         correct: 0
-    },
-    {
+    }, {
         text: '+0+0+0+0+0+0+0+2)+0+0+9)+7))+3)-0-0-0-0-0-0-0-9)+0+0+0+0+0+0+0+0+7)-8)+3)-6)-8)-7-0-0-0-0-0-0)',
         answers: [
             '0815',
@@ -18,8 +20,7 @@ var QUESTIONS = [
             'Barely'
         ],
         correct: 1
-    },
-    {
+    }, {
         text: '*6*3p*4*3*2*0p*2*1*0pp>0*1*0p*5*4*0p*5*4*2*1*0p*4*3p*1*0p/+0p+0*6*5*2p+0*5*0p',
         answers: [
             '0815',
@@ -28,8 +29,7 @@ var QUESTIONS = [
             'Barely'
         ],
         correct: 2
-    },
-    {
+    }, {
         text: ']xhhhhooooooooohhhhhhxooooooooxooooooxjjjxhoooohhhxhohhhhhhhxhhhhjjjhhhxhhhhooooooooohhhhhhxjjjxxjjjjjjjxjhhhhxjhhhhhhhhjjjhh~',
         answers: [
             '0815',
@@ -38,73 +38,69 @@ var QUESTIONS = [
             'Barely'
         ],
         correct: 3
-    }
-];
-
-var questionsPageElement = $('.questions-page');
-var questionCurrentElement = $('.question-current');
-var questionsTotalElement = $('.questions-total');
-var questionElement = $('.question');
-var answersElement = $('.answers');
-
-var resultsPageElement = $('.results-page');
-var scoreElement = $('.score');
-var restartButtonElement = $('.restart-button');
-
-var showResults = function() {
-    questionsPageElement.hide();
-    resultsPageElement.show();
+    }]
 };
 
-var showQuestions = function() {
-    resultsPageElement.hide();
-    questionsPageElement.show();
+//renders the state
+
+var showResults = function(questHide, resultReveal) {
+    questHide.hide();
+    resultReveal.show();
 };
 
-var resetScore = function() {
-    scoreElement.text(0);
+var showQuestions = function(questReveal, resultHide) {
+    resultHide.hide();
+    questReveal.show();
 };
 
-var increaseScore = function() {
-    var score = parseInt(scoreElement.text(), 10);
-    scoreElement.text(score + 1);
+var resetScore = function(scoreElem) {
+    scoreElem.text(0);
 };
 
-var setQuestion = function(questionIndex) {
-    var question = QUESTIONS[questionIndex];
+var increaseScore = function(scoreElem) {
+    var score = parseInt(scoreElem.text(), 10);
+    scoreElem.text(score + 1);
+};
+
+var setQuestion = function(state, questionIndex) {
+    var question = state.questions[questionIndex];
     questionCurrentElement.text(questionIndex);
     questionElement.text(question.text);
     answersElement.empty();
-    for (var i=0; i<question.answers.length; i++) {
+    for (var i = 0; i < question.answers.length; i++) {
         var answer = question.answers[i];
         answersElement.append('<li><button type="button">' + answer + '</button></li>');
     }
 };
 
-answersElement.on('click', 'button', function() {
+//event listners
+
+var questionCurrentElement = $('.question-current');
+var questionElement = $('.question');
+var answersElement = $('.answers');
+
+$('.answers').on('click', 'button', function() {
     var choice = $(this).parent().index();
-    var questionIndex = parseInt(questionCurrentElement.text(), 10);
-    var question = QUESTIONS[questionIndex];
+    var questionIndex = parseInt($('.question-current').text(), 10);
+    var question = state.questions[questionIndex];
     if (question.correct === choice) {
-        increaseScore();
+        increaseScore($('.score'));
     }
 
-    if (questionIndex + 1 < QUESTIONS.length) {
-        setQuestion(questionIndex + 1);
-    }
-    else {
-        showResults();
+    if (questionIndex + 1 < state.questions.length) {
+        setQuestion(state, questionIndex + 1);
+    } else {
+        showResults($('.questions-page'), $('.results-page'));
     }
 });
 
-restartButtonElement.click(function() {
-    setQuestion(0);
-    resetScore();
-    showQuestions();
+$('.restart-button').click(function() {
+    setQuestion(state, 0);
+    resetScore($('.score'));
+    showQuestions($('.questions-page'), $('.results-page'));
 });
 
 $(document).ready(function() {
-    questionsTotalElement.text(QUESTIONS.length);
-    setQuestion(0);
+    $('.questions-total').text(state.questions.length);
+    setQuestion(state, 0);
 });
-
